e3e90cf4d2a3349d8506ca640f5af0cd77fe02cc
LGNDLeviathan <roitchie@gmail.com>
2025-10-04 09:45:58 +0100

    Add placeholder MetalDetector class

---
+++ Assets/Scripts/MetalDetector.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MetalDetector : MonoBehaviour
+{
+    float range = 2f;
+    float battery = 100f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Detect()
+    {
+
+    }
+}
