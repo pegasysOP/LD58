ea59864df7a2cef91bbd6723fe241e29383c21e5
LGNDLeviathan <roitchie@gmail.com>
2025-10-04 09:57:43 +0100

    Added placeholder detect method

--- Assets/Scripts/MetalDetector.cs
+++ Assets/Scripts/MetalDetector.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MetalDetector : MonoBehaviour
 {
     float range = 2f;
     float battery = 100f;
+    private Keyboard keyboard;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        keyboard = Keyboard.current;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (keyboard.eKey.isPressed) Detect();
     }
 
-    void Detect()
+    public void Detect()
     {
-
+        Debug.Log("Detecting! Beep Beep Beep!");
     }
 }
