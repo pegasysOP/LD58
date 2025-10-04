afeeac8774d638f39f2c118b0050850656cbb428
LGNDLeviathan <roitchie@gmail.com>
2025-10-04 11:41:24 +0100

    Add detection of items in a sphere from metal detector

--- Assets/Scripts/Interactables/MetalDetector.cs
+++ Assets/Scripts/Interactables/MetalDetector.cs
@@ -3,9 +3,11 @@ using UnityEngine.InputSystem;
 
 public class MetalDetector : MonoBehaviour
 {
-    float range = 2f;
+    float range = 200f;
     float battery = 100f;
-    private Mouse mouse; 
+    private Mouse mouse;
+
+    public LayerMask metalDetectionMask;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +22,12 @@ public class MetalDetector : MonoBehaviour
 
     public void Detect()
     {
-        Debug.Log("Detecting! Beep Beep Beep!");
+        //TODO: Implement detecting. Find all objects with IInteractable and check their range
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, range, metalDetectionMask);
+        foreach (var hitCollider in hitColliders)
+        {
+            Debug.Log("Hit " + hitCollider.name);
+        } 
+
     }
 }
