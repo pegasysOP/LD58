d7397a96b51dd1613b36c360eb2672fe0bc5a9e9
LGNDLeviathan <roitchie@gmail.com>
2025-10-04 17:25:41 +0100

    Add placeholder upgrades for future upgrades

--- Assets/Scripts/Shop/Upgrades.cs
+++ Assets/Scripts/Shop/Upgrades.cs
@@ -6,11 +6,15 @@ public class Upgrades : MonoBehaviour
     public enum Upgrade
     {
         Battery1,
-        Battery2,
-        Battery3, 
         Range1,
-        Range2, 
-        Range3
+        Range2,
+        Backpack1,
+        WalletSize1,
+        Rarity1,
+        GoldDetector,
+        HeartbeatSensor,
+        Fossils, 
+        AlienTech
     }
 
     public Dictionary<Upgrade, float> upgradeCostDictionary = new Dictionary<Upgrade, float>();
@@ -18,10 +22,14 @@ public class Upgrades : MonoBehaviour
     void Start()
     {
         upgradeCostDictionary[Upgrade.Battery1] = 1f;
-        upgradeCostDictionary[Upgrade.Battery2] = 2f;
-        upgradeCostDictionary[Upgrade.Battery3] = 4f;
         upgradeCostDictionary[Upgrade.Range1] = 1f;
         upgradeCostDictionary[Upgrade.Range2] = 2f;
-        upgradeCostDictionary[Upgrade.Range3] = 4f;
+        upgradeCostDictionary[Upgrade.Backpack1] = 1f;
+        upgradeCostDictionary[Upgrade.WalletSize1] = 1f;
+        upgradeCostDictionary[Upgrade.Rarity1] = 1f;
+        upgradeCostDictionary[Upgrade.GoldDetector] = 1f;
+        upgradeCostDictionary[Upgrade.HeartbeatSensor] = 1f;
+        upgradeCostDictionary[Upgrade.Fossils] = 1f;
+        upgradeCostDictionary[Upgrade.AlienTech] = 1f;
     }
 }
