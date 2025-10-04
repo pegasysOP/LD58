6f6f2ef790ac2cee6a3ad8256809d2c3f62cc045
LGNDLeviathan <roitchie@gmail.com>
2025-10-04 20:57:35 +0100

    Add max angle and only return targets less than that angle from players forward direction

--- Assets/Scripts/Interactables/MetalDetector.cs
+++ Assets/Scripts/Interactables/MetalDetector.cs
@@ -1,12 +1,14 @@
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 public class MetalDetector : MonoBehaviour
 {
     [Header("Detection Settings")]
     public float range = 2f;
+    public float maxAngle = 90f;
     public LayerMask metalDetectionMask;
 
     [Header("Battery Settings")]
@@ -48,16 +50,36 @@ public class MetalDetector : MonoBehaviour
     public void Detect()
     {
         battery -= dischargeRate * Time.deltaTime;
-        if (battery <= 0f) return;
+        if (battery <= 0f) 
+            return;
 
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, metalDetectionMask);
+        
         if (hitColliders.Length == 0)
         {
             beepTimer = 0f;
             return;
         }
 
-        //TODO: Base this on direction too. I.e. only detect within a 45/90 degree angle or something.
+        Vector3 forward = transform.forward;
+        
+        //Get all targets less than max angle from players forward direction 
+        List<Collider> validTargets = hitColliders
+        .Where(hit =>
+        {
+            Vector3 directionToTarget = (hit.transform.position - transform.position).normalized;
+            float angle = Vector3.Angle(forward, directionToTarget);
+            return angle <= maxAngle;
+        })
+        .ToList();
+
+        if (validTargets.Count == 0)
+        {
+            beepTimer = 0f;
+            return;
+        }
+        
+        //Ensure valid targets are within range 
         float closestDistance = hitColliders
             .Select(hit => Vector3.Distance(transform.position, hit.transform.position))
             .Min();
