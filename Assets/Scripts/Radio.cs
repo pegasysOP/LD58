bb4e46fa1903746d71a19cc9bf7538f4a52fcc2f
LGNDLeviathan <roitchie@gmail.com>
2025-10-05 09:17:25 +0100

    Add playlist and switching to Radio

--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -1,18 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Radio : MonoBehaviour
 {
     AudioManager audioManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Keyboard keyboard;
+    private int currentSongIndex = 0;
+    private List<AudioClip> playlist;
+
     void Start()
     {
         audioManager = FindFirstObjectByType<AudioManager>();
         audioManager.PlayMusic(audioManager.hawiiSongClip, AudioManager.FadeType.None);
+        keyboard = Keyboard.current;
+
+        playlist = new List<AudioClip>()
+        {
+            AudioManager.Instance.hawiiSongClip,
+            AudioManager.Instance.jazzSongClip
+        };
+
+        AudioManager.Instance.PlayMusic(playlist[currentSongIndex], AudioManager.FadeType.None);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (keyboard.mKey.wasPressedThisFrame)
+        {
+            currentSongIndex = (currentSongIndex + 1) % playlist.Count;
+            //AudioManager.Instance.PlayMusic(playlist[currentSongIndex], AudioManager.FadeType.CrossFade);
+        }
     }
 }
